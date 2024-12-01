a423ff0e5c5f3a45247bf830a4f31f50f150f20f
Joe Shaw <joe@joeshaw.org>
2024-12-01 11:57:26 -0500

    tighten up the implementation a bit

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -2,49 +2,46 @@ use std::collections::HashMap;
 
 pub fn part1() {
     let input = include_str!("input.txt");
-    let mut col1: Vec<i32> = vec![];
-    let mut col2: Vec<i32> = vec![];
+    let mut col1: Vec<u32> = vec![];
+    let mut col2: Vec<u32> = vec![];
     for line in input.lines() {
-        let parts = line.split_whitespace().collect::<Vec<&str>>();
-        if parts.len() != 2 {
-            panic!("Unexpected line: {}", line);
-        }
-
-        col1.push(parts[0].parse().unwrap());
-        col2.push(parts[1].parse().unwrap());
+        let parts = line
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect::<Vec<u32>>();
+        col1.push(parts[0]);
+        col2.push(parts[1]);
     }
 
     col1.sort();
     col2.sort();
 
-    let mut distance: i32 = 0;
-
-    for i in 0..col1.len() {
-        distance += (col2[i] - col1[i]).abs();
-    }
-
+    let distance: u32 = col1
+        .into_iter()
+        .zip(col2)
+        .map(|(v1, v2)| v1.abs_diff(v2))
+        .sum();
     println!("Part 1 distance: {}", distance);
 }
 
 pub fn part2() {
     let input = include_str!("input.txt");
-    let mut col1: Vec<i32> = vec![];
-    let mut col2: HashMap<i32, i32> = HashMap::new();
+    let mut col1: Vec<u32> = vec![];
+    let mut col2: HashMap<u32, u32> = HashMap::new();
 
     for line in input.lines() {
-        let parts = line.split_whitespace().collect::<Vec<&str>>();
-        if parts.len() != 2 {
-            panic!("Unexpected line: {}", line);
-        }
+        let parts = line
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect::<Vec<u32>>();
 
-        col1.push(parts[0].parse().unwrap());
-        *col2.entry(parts[1].parse().unwrap()).or_default() += 1;
+        col1.push(parts[0]);
+        *col2.entry(parts[1]).or_default() += 1;
     }
 
-    let mut distance: i32 = 0;
-    for v in col1 {
-        distance += v * *col2.entry(v).or_default();
-    }
-
+    let distance: u32 = col1
+        .into_iter()
+        .map(|v| v * *col2.entry(v).or_default())
+        .sum();
     println!("Part 2 distance: {}", distance);
 }
