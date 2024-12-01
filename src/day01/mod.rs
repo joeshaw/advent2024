d0ac0631f451e1e7fa706904707edbcbc70593bc
Joe Shaw <joe@joeshaw.org>
2024-12-01 11:05:59 -0500

    day 1 part 2

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn part1() {
     let input = include_str!("input.txt");
     let mut col1: Vec<i32> = vec![];
@@ -21,5 +23,28 @@ pub fn part1() {
         distance += (col2[i] - col1[i]).abs();
     }
 
-    println!("Distance: {}", distance);
+    println!("Part 1 distance: {}", distance);
+}
+
+pub fn part2() {
+    let input = include_str!("input.txt");
+    let mut col1: Vec<i32> = vec![];
+    let mut col2: HashMap<i32, i32> = HashMap::new();
+
+    for line in input.lines() {
+        let parts = line.split_whitespace().collect::<Vec<&str>>();
+        if parts.len() != 2 {
+            panic!("Unexpected line: {}", line);
+        }
+
+        col1.push(parts[0].parse().unwrap());
+        *col2.entry(parts[1].parse().unwrap()).or_default() += 1;
+    }
+
+    let mut distance: i32 = 0;
+    for v in col1 {
+        distance += v * *col2.entry(v).or_default();
+    }
+
+    println!("Part 2 distance: {}", distance);
 }
